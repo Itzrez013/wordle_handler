df3abab53ee1afaf0a02c9a433e9679a1f8fb697
Reza <Itzrez013>
2026-09-16 17:25:46 +0330

    a little smoother

--- WordleHandler.py
+++ WordleHandler.py
@@ -1,10 +1,11 @@
+import os
+
 with open("wordle.txt") as f:
     words = [
         line.strip()
         for line in f
         if len(line.strip().lower()) == 5
     ]
-
 
 there_is = []
 
@@ -24,17 +25,13 @@ there_is_exact = {
     5: ""
 }
 
-
 def filter_words(words):
-
-
     for char in there_is:
         words = [
             word
             for word in words
             if char in word
         ]
-
 
 
     for position, chars in there_is_not.items():
@@ -63,15 +60,14 @@ def filter_words(words):
 
 while True:
 
-    inp = input("\nti , tin , tie , w , exit : ")
-
+    inp = input("\nti (there is) , tin (there is not) , tie (there is exact) , w (words) , clear , -h , exit , restart  : ")
+    w = True
 
 
     if inp == "ti":
 
-        char = input("there is a what? : ")
-
-        if char not in there_is:
+        char = input("there is a what? ( 0 for cancel ) : ")
+        if char not in there_is and char!="0" :
             there_is.append(char)
 
 
@@ -79,31 +75,57 @@ while True:
 
     elif inp == "tin":
 
-        char = input("there is what not? : ")
+        char = input("there is what not? ( 0 for cancel ) : ")
 
-        position = input("where? ( just enter for all indexes. ) : ")
+        if char!="0":
+            position = input("where? ( just enter for all indexes. ) ( 0 for cancel ) : ")
+            if position!='0':
+                if position=="":
 
+                    for i in range(1,6):
+                        there_is_not[i]+=char
 
-        if position=="":
-            for i in range(1,6):
-                there_is_not[i]+=char
+                else:
+                    position=int(position)
 
-        else:
-            position=int(position)
-
-        if position in there_is_not:
-            there_is_not[position] += char
+                if char and position in there_is_not:
+                    there_is_not[position] += char
 
 
 
     elif inp == "tie":
 
-        char = input("oh yeah? what is there? : ")
+        char = input("oh yeah? what is there? ( 0 for cancel ) : ")
 
-        position = int(input("really? where? : "))
+        if char!='0':
+            position = int(input("really? where? ( 0 for cancel ) : "))
+            if position in there_is_exact and position!='0':
+                there_is_exact[position] = char
 
-        if position in there_is_exact:
-            there_is_exact[position] = char
+
+
+    elif inp=="clear":
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+
+    elif inp=="restart":
+        there_is = []
+
+        there_is_not = {
+        1: "",
+        2: "",
+        3: "",
+        4: "",
+        5: ""
+        }
+
+        there_is_exact = {
+        1: "",
+        2: "",
+        3: "",
+        4: "",
+        5: ""
+        }
 
 
 
@@ -111,10 +133,18 @@ while True:
         break
 
 
+    elif inp=="-h":
+        print("""You can use 'ti' when you know a letter is in the word but don't know its exact position.
+You can use 'tin' when you're sure that a letter is not in a specific position.
+You can leave the index empty after using 'tin' to apply the rule to all positions.
+You can use 'tie' when you're sure about a letter at a specific position.
+You can use 'w' to see which words remain. """)
+
+
     elif inp == 'w':
         print("\nPossible words:")
         print(w2)
-
+        w=False
 
     else:
         print("Unknown command")
@@ -125,15 +155,15 @@ while True:
 
 
 
+    if w:
+        print("\nThere is:")
+        print(there_is)
 
-    print("\nThere is:")
-    print(there_is)
+        print("\nThere is NOT:")
+        print(there_is_not)
 
-    print("\nThere is NOT:")
-    print(there_is_not)
+        print("\nThere is EXACT:")
+        print(there_is_exact)
 
-    print("\nThere is EXACT:")
-    print(there_is_exact)
-
-    print("\nNumber of possible words:")
+        print("\nNumber of possible words:")
     print(len(w2))
